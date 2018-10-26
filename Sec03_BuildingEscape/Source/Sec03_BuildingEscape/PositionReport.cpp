edd7ed13d001fabfe3422393ce915d930141fb01
A. T. Campbell, III <acampbell@hookbang.com>
2018-10-26 17:08:25 -0500

    Work for Lectures 66-67: Implement PositionReport.

--- Sec03_BuildingEscape/Source/Sec03_BuildingEscape/PositionReport.cpp
+++ Sec03_BuildingEscape/Source/Sec03_BuildingEscape/PositionReport.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "GameFramework/Actor.h"
 #include "PositionReport.h"
 
 
@@ -10,7 +11,6 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -18,9 +18,8 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	FString objName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("UPositionReport BeginPlay on %s."), *objName);
 }
 
 
